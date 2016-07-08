V-Sekai-fire/multiplayer-fabric-godot

commit e2283b74019d6692d705b66b5f690e4610fc7d20

Removed unused variables (second pass) + dead code
Fixes various gcc 5.4.0 warnings for -Wunused-variable and -Wunused-but-set-variable

tools/editor/scenes_dock.cpp
@@ -37,6 +37,8 @@
 
 #include "editor_settings.h"
 #include "scene/main/viewport.h"
+
+
 bool ScenesDock::_create_tree(TreeItem *p_parent,EditorFileSystemDirectory *p_dir) {
 
 
@@ -56,61 +58,6 @@ bool ScenesDock::_create_tree(TreeItem *p_parent,EditorFileSystemDirectory *p_di
 	if (lpath==path) {
 		item->select(0);
 	}
-
-
-	//item->set_custom_bg_color(0,get_color("prop_subsection","Editor"));
-
-	bool has_items=false;
-
-	for(int i=0;i<p_dir->get_subdir_count();i++) {
-
-		if (_create_tree(item,p_dir->get_subdir(i)))
-			has_items=true;
-	}
-#if 0
-	for (int i=0;i<p_dir->get_file_count();i++) {
-
-		String file_name = p_dir->get_file(i);
-		String file_path = p_dir->get_file_path(i);
-
-		// ScenesDockFilter::FILTER_PATH
-		String search_from = file_path.right(6); // trim "res://"
-		if (file_filter == ScenesDockFilter::FILTER_NAME)
-			 search_from = file_name;
-		else if (file_filter == ScenesDockFilter::FILTER_FOLDER)
-			search_from = file_path.right(6).get_base_dir();
-
-		if (search_term!="" && search_from.findn(search_term)==-1)
-			continue;
-
-		bool isfave = favorites.has(file_path);
-		if (button_favorite->is_pressed() && !isfave)
-			continue;
-
-		TreeItem *fitem = tree->create_item(item);
-		fitem->set_cell_mode(0,TreeItem::CELL_MODE_CHECK);
-		fitem->set_editable(0,true);
-		fitem->set_checked(0,isfave);
-		fitem->set_text(0,file_name);
-
-		Ref<Texture> icon = get_icon( (has_icon(p_dir->get_file_type(i),"EditorIcons")?p_dir->get_file_type(i):String("Object")),"EditorIcons");
-		fitem->set_icon(0, icon );
-
-
-		fitem->set_metadata(0,file_path);
-		//if (p_dir->files[i]->icon.is_valid()) {
-//			fitem->set_icon(0,p_dir->files[i]->icon);
-//		}
-		has_items=true;
-
-	}
-#endif
-	/*if (!has_items) {
-
-		memdelete(item);
-		return false;
-
-	}*/
 
 	return true;
 }

drivers/chibi/cp_envelope.cpp
@@ -359,7 +359,6 @@ uint8_t CPEnvelope::get_node_count() {
 
 const CPEnvelope::Point& CPEnvelope::get_node(int p_idx) {
 	
-	static Point errpoint;
 	if (p_idx<0 || p_idx>=node_count)
 		return node[node_count-1];
 	

drivers/chibi/cp_loader_xm.cpp
@@ -560,8 +560,7 @@ CPLoader::Error CPLoader_XM::load_instrument_internal(CPInstrument *p_instr,bool
 			if (s_idx==-1) ABORT_LOAD;
 			//printf("free sample: %i\n",s_idx);
 			
-			
-			char auxb;
+
 			CPSample& sample=*song->get_sample(s_idx);
 			
 			int sample_size=file->get_dword();
@@ -611,6 +610,7 @@ CPLoader::Error CPLoader_XM::load_instrument_internal(CPInstrument *p_instr,bool
 			sm->set_c5_freq( sample_data, CPTables::get_linear_frequency(CPTables::get_linear_period(note_offset<<1,finetune)) );
 			//printf("NOTE %i,fine %i\n",note_offset,finetune);
 
+			char auxb;
 			auxb=file->get_byte(); //reserved?
 			file->get_byte_array((uint8_t*)instrname,22);
 			sample.set_name(instrname);

drivers/chibi/cp_player_data_events.cpp
@@ -325,8 +325,6 @@ void CPPlayer::update_mixer() {
 		printf("cgb %i\n",(int)v.master_channel->channel_global_volume);
 */
 
-
-		int cv=v.master_channel->channel_global_volume;
 		
 		tmpvol=(uint64_t)v.fadeout_volume;    /* max 1024 - 10 bits */
 		tmpvol*=(uint64_t)v.channel_volume;    /* * max 64 - 6 bits */
@@ -338,13 +336,9 @@ void CPPlayer::update_mixer() {
 		tmpvol*=(uint64_t)song->get_mixing_volume(); /* max 128 - 7 bits */
 		tmpvol*=(uint64_t)control.global_volume;   /* max 128 - 7 bits*/
 				/* total 10+6+8+6+7+7=44 bits */
-		uint64_t preshift=tmpvol;
+
 		tmpvol>>=43; /* Move back to 8 bits range , 44-19+8=43*/
 		
-		/*
-		if (!tmpvol && preshift>>35)
-			tmpvol=1; // even if the volume should be inaudible, some people WANTS to hear this.
-		*/
 		if (tmpvol>CP_VOL_MAX)
 			tmpvol=CP_VOL_MAX;
 		

drivers/openssl/stream_peer_openssl.cpp
@@ -469,7 +469,6 @@ Error StreamPeerOpenSSL::put_partial_data(const uint8_t* p_data,int p_bytes, int
 	if (p_bytes==0)
 		return OK;
 
-	int s=0;
 	Error err = put_data(p_data,p_bytes);
 	if (err!=OK)
 		return err;

drivers/speex/audio_stream_speex.cpp
@@ -69,8 +69,7 @@ int AudioStreamPlaybackSpeex::mix(int16_t* p_buffer,int p_frames) {
 		return 0;
 	};
 
-	int eos = 0;	
-	bool reloaded=false;
+	int eos = 0;
 
 	while (todo > page_size) {
 
@@ -100,7 +99,6 @@ int AudioStreamPlaybackSpeex::mix(int16_t* p_buffer,int p_frames) {
 				packets_available=true;
 			}
 			/*Extract all available packets*/
-			//int packet_no=0;
 			while (todo > page_size && !eos) {
 
 				if (ogg_stream_packetout(&os, &op)!=1) {
@@ -157,7 +155,6 @@ int AudioStreamPlaybackSpeex::mix(int16_t* p_buffer,int p_frames) {
 
 					{
 
-						int frame_offset = 0;
 						int new_frame_size = frame_size;
 
 						/*printf ("packet %d %d\n", packet_no, skip_samples);*/
@@ -165,7 +162,6 @@ int AudioStreamPlaybackSpeex::mix(int16_t* p_buffer,int p_frames) {
 						{
 							/*printf ("chopping first packet\n");*/
 							new_frame_size -= skip_samples;
-							frame_offset = skip_samples;
 						}
 						if (packet_no == page_nb_packets && skip_samples < 0)
 						{
@@ -241,10 +237,8 @@ void AudioStreamPlaybackSpeex::unload() {
 
 void *AudioStreamPlaybackSpeex::process_header(ogg_packet *op, int *frame_size, int *rate, int *nframes, int *channels, int *extra_headers) {
 
-	void *st;
 	SpeexHeader *header;
 	int modeID;
-	SpeexCallback callback;
 
 	header = speex_packet_to_header((char*)op->packet, op->bytes);
 	if (!header)
@@ -353,7 +347,6 @@ void AudioStreamPlaybackSpeex::reload() {
 		/*Loop for all complete pages we got (most likely only one)*/
 		while (ogg_sync_pageout(&oy, &og)==1) {
 
-			int packet_no;
 			if (stream_init == 0) {
 				ogg_stream_init(&os, ogg_page_serialno(&og));
 				stream_init = 1;
@@ -377,7 +370,6 @@ void AudioStreamPlaybackSpeex::reload() {
 
 			last_granule = page_granule;
 			/*Extract all available packets*/
-			packet_no=0;
 			while (!eos && ogg_stream_packetout(&os, &op)==1)
 			{
 				/*If first packet, process as Speex header*/

drivers/theora/video_stream_theora.cpp
@@ -81,12 +81,12 @@ int VideoStreamPlaybackTheora::queue_page(ogg_page *page){
 
 void VideoStreamPlaybackTheora::video_write(void){
 	th_ycbcr_buffer yuv;
-	int y_offset, uv_offset;
 	th_decode_ycbcr_out(td,yuv);
 
+	/*
+	int y_offset, uv_offset;
 	y_offset=(ti.pic_x&~1)+yuv[0].stride*(ti.pic_y&~1);
 
-	/*
 	{
 		int pixels = size.x * size.y;
 		frame_data.resize(pixels * 4);
@@ -117,7 +117,7 @@ void VideoStreamPlaybackTheora::video_write(void){
 		DVector<uint8_t>::Write w = frame_data.write();
 		char* dst = (char*)w.ptr();
 
-		uv_offset=(ti.pic_x/2)+(yuv[1].stride)*(ti.pic_y/2);
+		//uv_offset=(ti.pic_x/2)+(yuv[1].stride)*(ti.pic_y/2);
 
 		if (px_fmt == TH_PF_444) {
 
@@ -543,16 +543,10 @@ void VideoStreamPlaybackTheora::update(float p_delta) {
 	bool frame_done=false;
 	bool audio_done=!vorbis_p;
 
-	bool theora_done=false;
-
 	while (!frame_done || (!audio_done && !vorbis_eos)) {
 		//a frame needs to be produced
 
 		ogg_packet op;
-		bool audio_pending = false;
-
-
-		bool no_vorbis=false;
 		bool no_theora=false;
 
 
@@ -604,8 +598,6 @@ void VideoStreamPlaybackTheora::update(float p_delta) {
 
 				int tr = vorbis_synthesis_read(&vd, ret-to_read);
 
-				audio_pending=true;
-
 
 				if (vd.granulepos>=0) {
 				//	print_line("wrote: "+itos(audio_frames_wrote)+" gpos: "+itos(vd.granulepos));
@@ -627,7 +619,6 @@ void VideoStreamPlaybackTheora::update(float p_delta) {
 					}
 				} else {  /* we need more data; break out to suck in another page */
 					//printf("need moar data\n");
-					no_vorbis=true;
 					break;
 				};
 			}

platform/bb10/export/export.cpp
@@ -714,7 +714,6 @@ Error EditorExportPlatformBB10::run(int p_device, int p_flags) {
 	args.push_back("-installApp");
 	args.push_back("-launchApp");
 	args.push_back("-device");
-	int idx = devices[p_device].index;
 	String host = EditorSettings::get_singleton()->get("blackberry/device_"+itos(p_device+1)+"/host");
 	String pass = EditorSettings::get_singleton()->get("blackberry/device_"+itos(p_device+1)+"/password");
 	args.push_back(host);

commit 0e6ab3b759f082c57fb61c6a64957508b5310c6c

ScenesDock: Fix regression in tree generation
Was a regression from e2283b74019d6692d705b66b5f690e4610fc7d20.
Fixes #5590.

tools/editor/scenes_dock.cpp
@@ -59,6 +59,9 @@ bool ScenesDock::_create_tree(TreeItem *p_parent,EditorFileSystemDirectory *p_di
 		item->select(0);
 	}
 
+	for(int i=0;i<p_dir->get_subdir_count();i++)
+		_create_tree(item,p_dir->get_subdir(i));
+
 	return true;
 }
 