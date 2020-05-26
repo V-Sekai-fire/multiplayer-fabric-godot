bigmonte/godot-bigmonte

colorpicker margin bug fixed
Signed-off-by: bigmonte <34067397+bigmonte@users.noreply.github.com>

scene/gui/color_picker.cpp
@@ -70,7 +70,7 @@ void ColorPicker::_notification(int p_what) {
 		case NOTIFICATION_PARENTED: {
 
 			for (int i = 0; i < 4; i++)
-				set_margin((Margin)i, get_constant("margin"));
+				set_margin((Margin)i, get_margin((Margin)i) + get_constant("margin"));
 		} break;
 		case NOTIFICATION_VISIBILITY_CHANGED: {
 

V-Sekai-fire/multiplayer-fabric-godot

scene/gui/color_picker.cpp
@@ -66,7 +66,7 @@ void ColorPicker::_notification(int p_what) {
 		} break;
 		case NOTIFICATION_PARENTED: {
 			for (int i = 0; i < 4; i++) {
-				set_margin((Margin)i, get_theme_constant("margin"));
+				set_margin((Margin)i, get_margin((Margin)i) + get_theme_constant("margin"));
 			}
 		} break;
 		case NOTIFICATION_VISIBILITY_CHANGED: {