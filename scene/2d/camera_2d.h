KleirRampage45/redot-engine

fixed camera2D showing incorrect bounds in editor

scene/2d/camera_2d.h
@@ -148,6 +148,7 @@ public:
 	Vector2 get_zoom() const;
 
 	Point2 get_camera_screen_center() const;
+	Size2 get_camera_screen_size() const;
 
 	void set_custom_viewport(Node *p_viewport);
 	Node *get_custom_viewport() const;

V-Sekai-fire/multiplayer-fabric-godot

scene/2d/camera_2d.h
@@ -94,6 +94,8 @@ protected:
 
 	Camera2DProcessMode process_mode;
 
+	Size2 _get_camera_screen_size() const;
+
 protected:
 	virtual Transform2D get_camera_transform();
 	void _notification(int p_what);