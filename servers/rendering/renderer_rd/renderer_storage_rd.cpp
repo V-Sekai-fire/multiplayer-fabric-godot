BastiaanOlij/godot

Use fragment shader instead of compute shader for effects for mobile renderer

servers/rendering/renderer_rd/renderer_storage_rd.cpp
@@ -4962,7 +4962,7 @@ void RendererStorageRD::particles_set_view_axis(RID p_particles, const Vector3 &
 		RD::get_singleton()->compute_list_dispatch_threads(compute_list, particles->amount, 1, 1);
 
 		RD::get_singleton()->compute_list_end();
-		effects.sort_buffer(particles->particles_sort_uniform_set, particles->amount);
+		effects->sort_buffer(particles->particles_sort_uniform_set, particles->amount);
 	}
 
 	copy_push_constant.total_particles *= copy_push_constant.total_particles;
@@ -7538,7 +7538,7 @@ void RendererStorageRD::render_target_copy_to_back_buffer(RID p_render_target, c
 
 	//single texture copy for backbuffer
 	//RD::get_singleton()->texture_copy(rt->color, rt->backbuffer_mipmap0, Vector3(region.position.x, region.position.y, 0), Vector3(region.position.x, region.position.y, 0), Vector3(region.size.x, region.size.y, 1), 0, 0, 0, 0, true);
-	effects.copy_to_rect(rt->color, rt->backbuffer_mipmap0, region, false, false, false, true, true);
+	effects->copy_to_rect(rt->color, rt->backbuffer_mipmap0, region, false, false, false, true, true);
 
 	if (!p_gen_mipmaps) {
 		return;
@@ -7554,7 +7554,7 @@ void RendererStorageRD::render_target_copy_to_back_buffer(RID p_render_target, c
 		region.size.y = MAX(1, region.size.y >> 1);
 
 		const RenderTarget::BackbufferMipmap &mm = rt->backbuffer_mipmaps[i];
-		effects.gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
+		effects->gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
 		prev_texture = mm.mipmap;
 	}
 }
@@ -7577,7 +7577,7 @@ void RendererStorageRD::render_target_clear_back_buffer(RID p_render_target, con
 	}
 
 	//single texture copy for backbuffer
-	effects.set_color(rt->backbuffer_mipmap0, p_color, region, true);
+	effects->set_color(rt->backbuffer_mipmap0, p_color, region, true);
 }
 
 void RendererStorageRD::render_target_gen_back_buffer_mipmaps(RID p_render_target, const Rect2i &p_region) {
@@ -7607,7 +7607,7 @@ void RendererStorageRD::render_target_gen_back_buffer_mipmaps(RID p_render_targe
 		region.size.y = MAX(1, region.size.y >> 1);
 
 		const RenderTarget::BackbufferMipmap &mm = rt->backbuffer_mipmaps[i];
-		effects.gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
+		effects->gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
 		prev_texture = mm.mipmap;
 	}
 }
@@ -7928,14 +7928,14 @@ void RendererStorageRD::_update_decal_atlas() {
 				while ((K = decal_atlas.textures.next(K))) {
 					DecalAtlas::Texture *t = decal_atlas.textures.getptr(*K);
 					Texture *src_tex = texture_owner.getornull(*K);
-					effects.copy_to_atlas_fb(src_tex->rd_texture, mm.fb, t->uv_rect, draw_list, false, t->panorama_to_dp_users > 0);
+					effects->copy_to_atlas_fb(src_tex->rd_texture, mm.fb, t->uv_rect, draw_list, false, t->panorama_to_dp_users > 0);
 				}
 
 				RD::get_singleton()->draw_list_end();
 
 				prev_texture = mm.texture;
 			} else {
-				effects.copy_to_fb_rect(prev_texture, mm.fb, Rect2i(Point2i(), mm.size));
+				effects->copy_to_fb_rect(prev_texture, mm.fb, Rect2i(Point2i(), mm.size));
 				prev_texture = mm.texture;
 			}
 		} else {
@@ -8807,8 +8807,13 @@ bool RendererStorageRD::free(RID p_rid) {
 	return true;
 }
 
+void RendererStorageRD::init_effects(bool p_can_use_compute) {
+	effects = memnew(EffectsRD(p_can_use_compute));
+}
+
 EffectsRD *RendererStorageRD::get_effects() {
-	return &effects;
+	ERR_FAIL_NULL_V_MSG(effects, nullptr, "Effects haven't been initialised yet.");
+	return effects;
 }
 
 void RendererStorageRD::capture_timestamps_begin() {
@@ -9531,5 +9536,10 @@ RendererStorageRD::~RendererStorageRD() {
 
 	if (decal_atlas.texture.is_valid()) {
 		RD::get_singleton()->free(decal_atlas.texture);
+	}
+
+	if (effects) {
+		memdelete(effects);
+		effects = NULL;
 	}
 }

servers/rendering/renderer_rd/renderer_storage_rd.h
@@ -1290,7 +1290,7 @@ private:
 	void _update_global_variables();
 	/* EFFECTS */
 
-	EffectsRD effects;
+	EffectsRD *effects = NULL;
 
 public:
 	virtual bool can_create_resources_async() const;
@@ -2374,6 +2374,7 @@ public:
 
 	static RendererStorageRD *base_singleton;
 
+	void init_effects(bool p_can_use_compute);
 	EffectsRD *get_effects();
 
 	RendererStorageRD();

V-Sekai-fire/multiplayer-fabric-godot

servers/rendering/renderer_rd/renderer_storage_rd.cpp
@@ -4962,7 +4962,7 @@ void RendererStorageRD::particles_set_view_axis(RID p_particles, const Vector3 &
 		RD::get_singleton()->compute_list_dispatch_threads(compute_list, particles->amount, 1, 1);
 
 		RD::get_singleton()->compute_list_end();
-		effects.sort_buffer(particles->particles_sort_uniform_set, particles->amount);
+		effects->sort_buffer(particles->particles_sort_uniform_set, particles->amount);
 	}
 
 	copy_push_constant.total_particles *= copy_push_constant.total_particles;
@@ -7538,7 +7538,7 @@ void RendererStorageRD::render_target_copy_to_back_buffer(RID p_render_target, c
 
 	//single texture copy for backbuffer
 	//RD::get_singleton()->texture_copy(rt->color, rt->backbuffer_mipmap0, Vector3(region.position.x, region.position.y, 0), Vector3(region.position.x, region.position.y, 0), Vector3(region.size.x, region.size.y, 1), 0, 0, 0, 0, true);
-	effects.copy_to_rect(rt->color, rt->backbuffer_mipmap0, region, false, false, false, true, true);
+	effects->copy_to_rect(rt->color, rt->backbuffer_mipmap0, region, false, false, false, true, true);
 
 	if (!p_gen_mipmaps) {
 		return;
@@ -7554,7 +7554,7 @@ void RendererStorageRD::render_target_copy_to_back_buffer(RID p_render_target, c
 		region.size.y = MAX(1, region.size.y >> 1);
 
 		const RenderTarget::BackbufferMipmap &mm = rt->backbuffer_mipmaps[i];
-		effects.gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
+		effects->gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
 		prev_texture = mm.mipmap;
 	}
 }
@@ -7577,7 +7577,7 @@ void RendererStorageRD::render_target_clear_back_buffer(RID p_render_target, con
 	}
 
 	//single texture copy for backbuffer
-	effects.set_color(rt->backbuffer_mipmap0, p_color, region, true);
+	effects->set_color(rt->backbuffer_mipmap0, p_color, region, true);
 }
 
 void RendererStorageRD::render_target_gen_back_buffer_mipmaps(RID p_render_target, const Rect2i &p_region) {
@@ -7607,7 +7607,7 @@ void RendererStorageRD::render_target_gen_back_buffer_mipmaps(RID p_render_targe
 		region.size.y = MAX(1, region.size.y >> 1);
 
 		const RenderTarget::BackbufferMipmap &mm = rt->backbuffer_mipmaps[i];
-		effects.gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
+		effects->gaussian_blur(prev_texture, mm.mipmap, mm.mipmap_copy, region, true);
 		prev_texture = mm.mipmap;
 	}
 }
@@ -7928,14 +7928,14 @@ void RendererStorageRD::_update_decal_atlas() {
 				while ((K = decal_atlas.textures.next(K))) {
 					DecalAtlas::Texture *t = decal_atlas.textures.getptr(*K);
 					Texture *src_tex = texture_owner.getornull(*K);
-					effects.copy_to_atlas_fb(src_tex->rd_texture, mm.fb, t->uv_rect, draw_list, false, t->panorama_to_dp_users > 0);
+					effects->copy_to_atlas_fb(src_tex->rd_texture, mm.fb, t->uv_rect, draw_list, false, t->panorama_to_dp_users > 0);
 				}
 
 				RD::get_singleton()->draw_list_end();
 
 				prev_texture = mm.texture;
 			} else {
-				effects.copy_to_fb_rect(prev_texture, mm.fb, Rect2i(Point2i(), mm.size));
+				effects->copy_to_fb_rect(prev_texture, mm.fb, Rect2i(Point2i(), mm.size));
 				prev_texture = mm.texture;
 			}
 		} else {
@@ -8807,8 +8807,13 @@ bool RendererStorageRD::free(RID p_rid) {
 	return true;
 }
 
+void RendererStorageRD::init_effects(bool p_prefer_raster_effects) {
+	effects = memnew(EffectsRD(p_prefer_raster_effects));
+}
+
 EffectsRD *RendererStorageRD::get_effects() {
-	return &effects;
+	ERR_FAIL_NULL_V_MSG(effects, nullptr, "Effects haven't been initialised yet.");
+	return effects;
 }
 
 void RendererStorageRD::capture_timestamps_begin() {
@@ -9531,5 +9536,10 @@ RendererStorageRD::~RendererStorageRD() {
 
 	if (decal_atlas.texture.is_valid()) {
 		RD::get_singleton()->free(decal_atlas.texture);
+	}
+
+	if (effects) {
+		memdelete(effects);
+		effects = NULL;
 	}
 }

servers/rendering/renderer_rd/renderer_storage_rd.h
@@ -1290,7 +1290,7 @@ private:
 	void _update_global_variables();
 	/* EFFECTS */
 
-	EffectsRD effects;
+	EffectsRD *effects = NULL;
 
 public:
 	virtual bool can_create_resources_async() const;
@@ -2374,6 +2374,7 @@ public:
 
 	static RendererStorageRD *base_singleton;
 
+	void init_effects(bool p_prefer_raster_effects);
 	EffectsRD *get_effects();
 
 	RendererStorageRD();