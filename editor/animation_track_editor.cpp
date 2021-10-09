godotengine/godot

implement ping-pong loop in animation

editor/animation_track_editor.cpp
@@ -1328,8 +1328,20 @@ void AnimationTimelineEdit::_anim_length_changed(double p_new_len) {
 
 void AnimationTimelineEdit::_anim_loop_pressed() {
 	undo_redo->create_action(TTR("Change Animation Loop"));
-	undo_redo->add_do_method(animation.ptr(), "set_loop", loop->is_pressed());
-	undo_redo->add_undo_method(animation.ptr(), "set_loop", animation->has_loop());
+	switch (animation->get_loop_mode()) {
+		case Animation::LoopMode::LOOP_NONE: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_LINEAR);
+		} break;
+		case Animation::LoopMode::LOOP_LINEAR: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_PING_PONG);
+		} break;
+		case Animation::LoopMode::LOOP_PING_PONG: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_NONE);
+		} break;
+		default:
+			break;
+	}
+	undo_redo->add_undo_method(animation.ptr(), "set_loop_mode", animation->get_loop_mode());
 	undo_redo->commit_action();
 }
 
@@ -1616,7 +1628,24 @@ void AnimationTimelineEdit::update_values() {
 		length->set_tooltip(TTR("Animation length (seconds)"));
 		time_icon->set_tooltip(TTR("Animation length (seconds)"));
 	}
-	loop->set_pressed(animation->has_loop());
+
+	switch (animation->get_loop_mode()) {
+		case Animation::LoopMode::LOOP_NONE: {
+			loop->set_icon(get_theme_icon("Loop", "EditorIcons"));
+			loop->set_pressed(false);
+		} break;
+		case Animation::LoopMode::LOOP_LINEAR: {
+			loop->set_icon(get_theme_icon("Loop", "EditorIcons"));
+			loop->set_pressed(true);
+		} break;
+		case Animation::LoopMode::LOOP_PING_PONG: {
+			loop->set_icon(get_theme_icon("PingPongLoop", "EditorIcons"));
+			loop->set_pressed(true);
+		} break;
+		default:
+			break;
+	}
+
 	editing = false;
 }
 
@@ -2036,25 +2065,25 @@ void AnimationTrackEdit::_notification(int p_what) {
 
 				Ref<Texture2D> icon = wrap_icon[loop_wrap ? 1 : 0];
 
-				loop_mode_rect.position.x = ofs;
-				loop_mode_rect.position.y = int(get_size().height - icon->get_height()) / 2;
-				loop_mode_rect.size = icon->get_size();
+				loop_wrap_rect.position.x = ofs;
+				loop_wrap_rect.position.y = int(get_size().height - icon->get_height()) / 2;
+				loop_wrap_rect.size = icon->get_size();
 
 				if (animation->track_get_type(track) == Animation::TYPE_VALUE || animation->track_get_type(track) == Animation::TYPE_TRANSFORM3D) {
-					draw_texture(icon, loop_mode_rect.position);
+					draw_texture(icon, loop_wrap_rect.position);
 				}
 
-				loop_mode_rect.position.y = 0;
-				loop_mode_rect.size.y = get_size().height;
+				loop_wrap_rect.position.y = 0;
+				loop_wrap_rect.size.y = get_size().height;
 
 				ofs += icon->get_width() + hsep;
-				loop_mode_rect.size.x += hsep;
+				loop_wrap_rect.size.x += hsep;
 
 				if (animation->track_get_type(track) == Animation::TYPE_VALUE || animation->track_get_type(track) == Animation::TYPE_TRANSFORM3D) {
 					draw_texture(down_icon, Vector2(ofs, int(get_size().height - down_icon->get_height()) / 2));
-					loop_mode_rect.size.x += down_icon->get_width();
+					loop_wrap_rect.size.x += down_icon->get_width();
 				} else {
-					loop_mode_rect = Rect2();
+					loop_wrap_rect = Rect2();
 				}
 
 				ofs += down_icon->get_width();
@@ -2400,7 +2429,7 @@ String AnimationTrackEdit::get_tooltip(const Point2 &p_pos) const {
 		return TTR("Interpolation Mode");
 	}
 
-	if (loop_mode_rect.has_point(p_pos)) {
+	if (loop_wrap_rect.has_point(p_pos)) {
 		return TTR("Loop Wrap Mode (Interpolate end with beginning on loop)");
 	}
 
@@ -2599,7 +2628,7 @@ void AnimationTrackEdit::_gui_input(const Ref<InputEvent> &p_event) {
 			accept_event();
 		}
 
-		if (loop_mode_rect.has_point(pos)) {
+		if (loop_wrap_rect.has_point(pos)) {
 			if (!menu) {
 				menu = memnew(PopupMenu);
 				add_child(menu);
@@ -2610,7 +2639,7 @@ void AnimationTrackEdit::_gui_input(const Ref<InputEvent> &p_event) {
 			menu->add_icon_item(get_theme_icon("InterpWrapLoop", "EditorIcons"), TTR("Wrap Loop Interp"), MENU_LOOP_WRAP);
 			menu->set_as_minsize();
 
-			Vector2 popup_pos = get_screen_position() + loop_mode_rect.position + Vector2(0, loop_mode_rect.size.height);
+			Vector2 popup_pos = get_screen_position() + loop_wrap_rect.position + Vector2(0, loop_wrap_rect.size.height);
 			menu->set_position(popup_pos);
 			menu->popup();
 			accept_event();

scene/animation/animation_blend_tree.cpp
@@ -30,6 +30,7 @@
 
 #include "animation_blend_tree.h"
 
+#include "scene/resources/animation.h"
 #include "scene/scene_string_names.h"
 
 void AnimationNodeAnimation::set_animation(const StringName &p_name) {
@@ -83,30 +84,55 @@ float AnimationNodeAnimation::process(float p_time, bool p_seek) {
 	}
 
 	Ref<Animation> anim = ap->get_animation(animation);
-
-	float step;
+	float anim_size = anim->get_length();
+	float step = 0.0;
+	float prev_time = time;
+	int pingpong = 0;
+	bool current_backward = signbit(p_time);
 
 	if (p_seek) {
+		step = p_time - time;
 		time = p_time;
-		step = 0;
 	} else {
-		time = MAX(0, time + p_time);
-		step = p_time;
-	}
-
-	float anim_size = anim->get_length();
-
-	if (anim->has_loop()) {
-		if (anim_size) {
-			time = Math::fposmod(time, anim_size);
+		p_time *= backward ? -1.0 : 1.0;
+		if (!(time == anim_size && !current_backward) && !(time == 0 && current_backward)) {
+			time = time + p_time;
+			step = p_time;
 		}
-
-	} else if (time > anim_size) {
-		time = anim_size;
 	}
 
-	blend_animation(animation, time, step, p_seek, 1.0);
+	if (anim->get_loop_mode() == Animation::LoopMode::LOOP_PING_PONG) {
+		if (anim_size) {
+			if ((int)Math::floor(abs(time - prev_time) / anim_size) % 2 == 0) {
+				if (prev_time > 0 && time <= 0) {
+					backward = !backward;
+					pingpong = -1;
+				}
+				if (prev_time < anim_size && time >= anim_size) {
+					backward = !backward;
+					pingpong = 1;
+				}
+			}
+			time = Math::pingpong(time, anim_size);
+		}
+	} else {
+		if (anim->get_loop_mode() == Animation::LoopMode::LOOP_LINEAR) {
+			if (anim_size) {
+				time = Math::fposmod(time, anim_size);
+			}
+		} else if (time < 0) {
+			time = 0;
+		} else if (time > anim_size) {
+			time = anim_size;
+		}
+		backward = false;
+	}
 
+	if (play_mode == PLAY_MODE_FORWARD) {
+		blend_animation(animation, time, step, p_seek, 1.0, pingpong);
+	} else {
+		blend_animation(animation, anim_size - time, -step, p_seek, 1.0, pingpong);
+	}
 	set_parameter(this->time, time);
 
 	return anim_size - time;
@@ -116,11 +142,34 @@ String AnimationNodeAnimation::get_caption() const {
 	return "Animation";
 }
 
+void AnimationNodeAnimation::set_play_mode(PlayMode p_play_mode) {
+	play_mode = p_play_mode;
+}
+
+AnimationNodeAnimation::PlayMode AnimationNodeAnimation::get_play_mode() const {
+	return play_mode;
+}
+
+void AnimationNodeAnimation::set_backward(bool p_backward) {
+	backward = p_backward;
+}
+
+bool AnimationNodeAnimation::is_backward() const {
+	return backward;
+}
+
 void AnimationNodeAnimation::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_animation", "name"), &AnimationNodeAnimation::set_animation);
 	ClassDB::bind_method(D_METHOD("get_animation"), &AnimationNodeAnimation::get_animation);
 
+	ClassDB::bind_method(D_METHOD("set_play_mode", "mode"), &AnimationNodeAnimation::set_play_mode);
+	ClassDB::bind_method(D_METHOD("get_play_mode"), &AnimationNodeAnimation::get_play_mode);
+
 	ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "animation"), "set_animation", "get_animation");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "play_mode", PROPERTY_HINT_ENUM, "Forward,Backward"), "set_play_mode", "get_play_mode");
+
+	BIND_ENUM_CONSTANT(PLAY_MODE_FORWARD);
+	BIND_ENUM_CONSTANT(PLAY_MODE_BACKWARD);
 }
 
 AnimationNodeAnimation::AnimationNodeAnimation() {
@@ -534,7 +583,7 @@ AnimationNodeBlend3::AnimationNodeBlend3() {
 /////////////////////////////////
 
 void AnimationNodeTimeScale::get_parameter_list(List<PropertyInfo> *r_list) const {
-	r_list->push_back(PropertyInfo(Variant::FLOAT, scale, PROPERTY_HINT_RANGE, "0,32,0.01,or_greater"));
+	r_list->push_back(PropertyInfo(Variant::FLOAT, scale, PROPERTY_HINT_RANGE, "-32,32,0.01,or_greater"));
 }
 
 Variant AnimationNodeTimeScale::get_parameter_default_value(const StringName &p_parameter) const {

scene/animation/animation_player.h
@@ -215,7 +215,7 @@ private:
 
 	NodePath root;
 
-	void _animation_process_animation(AnimationData *p_anim, float p_time, float p_delta, float p_interp, bool p_is_current = true, bool p_seeked = false, bool p_started = false);
+	void _animation_process_animation(AnimationData *p_anim, float p_time, float p_delta, float p_interp, bool p_is_current = true, bool p_seeked = false, bool p_started = false, int p_pingpong = 0);
 
 	void _ensure_node_caches(AnimationData *p_anim, Node *p_root_override = nullptr);
 	void _animation_process_data(PlaybackData &cd, float p_delta, float p_blend, bool p_seeked, bool p_started);

scene/resources/animation.h
@@ -60,7 +60,12 @@ public:
 		UPDATE_DISCRETE,
 		UPDATE_TRIGGER,
 		UPDATE_CAPTURE,
+	};
 
+	enum LoopMode {
+		LOOP_NONE,
+		LOOP_LINEAR,
+		LOOP_PING_PONG,
 	};
 
 private:
@@ -181,7 +186,7 @@ private:
 	int _insert(float p_time, T &p_keys, const V &p_value);
 
 	template <class K>
-	inline int _find(const Vector<K> &p_keys, float p_time) const;
+	inline int _find(const Vector<K> &p_keys, float p_time, bool p_backward = false) const;
 
 	_FORCE_INLINE_ Animation::TransformKey _interpolate(const Animation::TransformKey &p_a, const Animation::TransformKey &p_b, float p_c) const;
 
@@ -197,7 +202,7 @@ private:
 	_FORCE_INLINE_ float _cubic_interpolate(const float &p_pre_a, const float &p_a, const float &p_b, const float &p_post_b, float p_c) const;
 
 	template <class T>
-	_FORCE_INLINE_ T _interpolate(const Vector<TKey<T>> &p_keys, float p_time, InterpolationType p_interp, bool p_loop_wrap, bool *p_ok) const;
+	_FORCE_INLINE_ T _interpolate(const Vector<TKey<T>> &p_keys, float p_time, InterpolationType p_interp, bool p_loop_wrap, bool *p_ok, bool p_backward = false) const;
 
 	template <class T>
 	_FORCE_INLINE_ void _track_get_key_indices_in_range(const Vector<T> &p_array, float from_time, float to_time, List<int> *p_indices) const;
@@ -207,15 +212,16 @@ private:
 
 	float length = 1.0;
 	float step = 0.1;
-	bool loop = false;
+	LoopMode loop_mode = LOOP_NONE;
+	int pingponged = 0;
 
 	// bind helpers
 private:
-	Array _transform_track_interpolate(int p_track, float p_time) const {
+	Array _transform_track_interpolate(int p_track, float p_time, bool p_backward = false) const {
 		Vector3 loc;
 		Quaternion rot;
 		Vector3 scale;
-		transform_track_interpolate(p_track, p_time, &loc, &rot, &scale);
+		transform_track_interpolate(p_track, p_time, &loc, &rot, &scale, p_backward);
 		Array ret;
 		ret.push_back(loc);
 		ret.push_back(rot);
@@ -321,26 +327,26 @@ public:
 	void track_set_interpolation_loop_wrap(int p_track, bool p_enable);
 	bool track_get_interpolation_loop_wrap(int p_track) const;
 
-	Error transform_track_interpolate(int p_track, float p_time, Vector3 *r_loc, Quaternion *r_rot, Vector3 *r_scale) const;
+	Error transform_track_interpolate(int p_track, float p_time, Vector3 *r_loc, Quaternion *r_rot, Vector3 *r_scale, bool p_backward = false) const;
 
 	Variant value_track_interpolate(int p_track, float p_time) const;
-	void value_track_get_key_indices(int p_track, float p_time, float p_delta, List<int> *p_indices) const;
+	void value_track_get_key_indices(int p_track, float p_time, float p_delta, List<int> *p_indices, int p_pingpong = 0) const;
 	void value_track_set_update_mode(int p_track, UpdateMode p_mode);
 	UpdateMode value_track_get_update_mode(int p_track) const;
 
-	void method_track_get_key_indices(int p_track, float p_time, float p_delta, List<int> *p_indices) const;
+	void method_track_get_key_indices(int p_track, float p_time, float p_delta, List<int> *p_indices, int p_pingpong = 0) const;
 	Vector<Variant> method_track_get_params(int p_track, int p_key_idx) const;
 	StringName method_track_get_name(int p_track, int p_key_idx) const;
 
 	void copy_track(int p_track, Ref<Animation> p_to_animation);
 
-	void track_get_key_indices_in_range(int p_track, float p_time, float p_delta, List<int> *p_indices) const;
+	void track_get_key_indices_in_range(int p_track, float p_time, float p_delta, List<int> *p_indices, int p_pingpong = 0) const;
 
 	void set_length(float p_length);
 	float get_length() const;
 
-	void set_loop(bool p_enabled);
-	bool has_loop() const;
+	void set_loop_mode(LoopMode p_loop_mode);
+	LoopMode get_loop_mode() const;
 
 	void set_step(float p_step);
 	float get_step() const;
@@ -356,5 +362,6 @@ public:
 VARIANT_ENUM_CAST(Animation::TrackType);
 VARIANT_ENUM_CAST(Animation::InterpolationType);
 VARIANT_ENUM_CAST(Animation::UpdateMode);
+VARIANT_ENUM_CAST(Animation::LoopMode);
 
 #endif

V-Sekai-fire/multiplayer-fabric-godot

Co-authored-by: Chaosus <chaosus89@gmail.com>

editor/animation_track_editor.cpp
@@ -1323,8 +1323,20 @@ void AnimationTimelineEdit::_anim_length_changed(double p_new_len) {
 
 void AnimationTimelineEdit::_anim_loop_pressed() {
 	undo_redo->create_action(TTR("Change Animation Loop"));
-	undo_redo->add_do_method(animation.ptr(), "set_loop", loop->is_pressed());
-	undo_redo->add_undo_method(animation.ptr(), "set_loop", animation->has_loop());
+	switch (animation->get_loop_mode()) {
+		case Animation::LoopMode::LOOP_NONE: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_LINEAR);
+		} break;
+		case Animation::LoopMode::LOOP_LINEAR: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_PINGPONG);
+		} break;
+		case Animation::LoopMode::LOOP_PINGPONG: {
+			undo_redo->add_do_method(animation.ptr(), "set_loop_mode", Animation::LoopMode::LOOP_NONE);
+		} break;
+		default:
+			break;
+	}
+	undo_redo->add_undo_method(animation.ptr(), "set_loop_mode", animation->get_loop_mode());
 	undo_redo->commit_action();
 }
 
@@ -1607,7 +1619,24 @@ void AnimationTimelineEdit::update_values() {
 		length->set_tooltip(TTR("Animation length (seconds)"));
 		time_icon->set_tooltip(TTR("Animation length (seconds)"));
 	}
-	loop->set_pressed(animation->has_loop());
+
+	switch (animation->get_loop_mode()) {
+		case Animation::LoopMode::LOOP_NONE: {
+			loop->set_icon(get_theme_icon("Loop", "EditorIcons"));
+			loop->set_pressed(false);
+		} break;
+		case Animation::LoopMode::LOOP_LINEAR: {
+			loop->set_icon(get_theme_icon("Loop", "EditorIcons"));
+			loop->set_pressed(true);
+		} break;
+		case Animation::LoopMode::LOOP_PINGPONG: {
+			loop->set_icon(get_theme_icon("PingPongLoop", "EditorIcons"));
+			loop->set_pressed(true);
+		} break;
+		default:
+			break;
+	}
+
 	editing = false;
 }
 
@@ -2050,25 +2079,25 @@ void AnimationTrackEdit::_notification(int p_what) {
 
 				Ref<Texture2D> icon = wrap_icon[loop_wrap ? 1 : 0];
 
-				loop_mode_rect.position.x = ofs;
-				loop_mode_rect.position.y = int(get_size().height - icon->get_height()) / 2;
-				loop_mode_rect.size = icon->get_size();
+				loop_wrap_rect.position.x = ofs;
+				loop_wrap_rect.position.y = int(get_size().height - icon->get_height()) / 2;
+				loop_wrap_rect.size = icon->get_size();
 
 				if (animation->track_get_type(track) == Animation::TYPE_VALUE || animation->track_get_type(track) == Animation::TYPE_TRANSFORM3D) {
-					draw_texture(icon, loop_mode_rect.position);
+					draw_texture(icon, loop_wrap_rect.position);
 				}
 
-				loop_mode_rect.position.y = 0;
-				loop_mode_rect.size.y = get_size().height;
+				loop_wrap_rect.position.y = 0;
+				loop_wrap_rect.size.y = get_size().height;
 
 				ofs += icon->get_width() + hsep;
-				loop_mode_rect.size.x += hsep;
+				loop_wrap_rect.size.x += hsep;
 
 				if (animation->track_get_type(track) == Animation::TYPE_VALUE || animation->track_get_type(track) == Animation::TYPE_TRANSFORM3D) {
 					draw_texture(down_icon, Vector2(ofs, int(get_size().height - down_icon->get_height()) / 2));
-					loop_mode_rect.size.x += down_icon->get_width();
+					loop_wrap_rect.size.x += down_icon->get_width();
 				} else {
-					loop_mode_rect = Rect2();
+					loop_wrap_rect = Rect2();
 				}
 
 				ofs += down_icon->get_width();
@@ -2415,7 +2444,7 @@ String AnimationTrackEdit::get_tooltip(const Point2 &p_pos) const {
 		return TTR("Interpolation Mode");
 	}
 
-	if (loop_mode_rect.has_point(p_pos)) {
+	if (loop_wrap_rect.has_point(p_pos)) {
 		return TTR("Loop Wrap Mode (Interpolate end with beginning on loop)");
 	}
 
@@ -2614,7 +2643,7 @@ void AnimationTrackEdit::gui_input(const Ref<InputEvent> &p_event) {
 			accept_event();
 		}
 
-		if (loop_mode_rect.has_point(pos)) {
+		if (loop_wrap_rect.has_point(pos)) {
 			if (!menu) {
 				menu = memnew(PopupMenu);
 				add_child(menu);
@@ -2625,7 +2654,7 @@ void AnimationTrackEdit::gui_input(const Ref<InputEvent> &p_event) {
 			menu->add_icon_item(get_theme_icon(SNAME("InterpWrapLoop"), SNAME("EditorIcons")), TTR("Wrap Loop Interp"), MENU_LOOP_WRAP);
 			menu->set_as_minsize();
 
-			Vector2 popup_pos = get_screen_position() + loop_mode_rect.position + Vector2(0, loop_mode_rect.size.height);
+			Vector2 popup_pos = get_screen_position() + loop_wrap_rect.position + Vector2(0, loop_wrap_rect.size.height);
 			menu->set_position(popup_pos);
 			menu->popup();
 			accept_event();

scene/animation/animation_blend_tree.cpp
@@ -30,6 +30,7 @@
 
 #include "animation_blend_tree.h"
 
+#include "scene/resources/animation.h"
 #include "scene/scene_string_names.h"
 
 void AnimationNodeAnimation::set_animation(const StringName &p_name) {
@@ -83,30 +84,55 @@ double AnimationNodeAnimation::process(double p_time, bool p_seek) {
 	}
 
 	Ref<Animation> anim = ap->get_animation(animation);
-
-	double step;
+	double anim_size = (double)anim->get_length();
+	double step = 0.0;
+	double prev_time = time;
+	int pingponged = 0;
+	bool current_backward = signbit(p_time);
 
 	if (p_seek) {
+		step = p_time - time;
 		time = p_time;
-		step = 0;
 	} else {
-		time = MAX(0, time + p_time);
-		step = p_time;
-	}
-
-	double anim_size = anim->get_length();
-
-	if (anim->has_loop()) {
-		if (anim_size) {
-			time = Math::fposmod(time, anim_size);
+		p_time *= backward ? -1.0 : 1.0;
+		if (!(time == anim_size && !current_backward) && !(time == 0 && current_backward)) {
+			time = time + p_time;
+			step = p_time;
 		}
-
-	} else if (time > anim_size) {
-		time = anim_size;
 	}
 
-	blend_animation(animation, time, step, p_seek, 1.0);
+	if (anim->get_loop_mode() == Animation::LoopMode::LOOP_PINGPONG) {
+		if (anim_size) {
+			if ((int)Math::floor(abs(time - prev_time) / anim_size) % 2 == 0) {
+				if (prev_time > 0 && time <= 0) {
+					backward = !backward;
+					pingponged = -1;
+				}
+				if (prev_time < anim_size && time >= anim_size) {
+					backward = !backward;
+					pingponged = 1;
+				}
+			}
+			time = Math::pingpong(time, anim_size);
+		}
+	} else {
+		if (anim->get_loop_mode() == Animation::LoopMode::LOOP_LINEAR) {
+			if (anim_size) {
+				time = Math::fposmod(time, anim_size);
+			}
+		} else if (time < 0) {
+			time = 0;
+		} else if (time > anim_size) {
+			time = anim_size;
+		}
+		backward = false;
+	}
 
+	if (play_mode == PLAY_MODE_FORWARD) {
+		blend_animation(animation, time, step, p_seek, 1.0, pingponged);
+	} else {
+		blend_animation(animation, anim_size - time, -step, p_seek, 1.0, pingponged);
+	}
 	set_parameter(this->time, time);
 
 	return anim_size - time;
@@ -116,11 +142,34 @@ String AnimationNodeAnimation::get_caption() const {
 	return "Animation";
 }
 
+void AnimationNodeAnimation::set_play_mode(PlayMode p_play_mode) {
+	play_mode = p_play_mode;
+}
+
+AnimationNodeAnimation::PlayMode AnimationNodeAnimation::get_play_mode() const {
+	return play_mode;
+}
+
+void AnimationNodeAnimation::set_backward(bool p_backward) {
+	backward = p_backward;
+}
+
+bool AnimationNodeAnimation::is_backward() const {
+	return backward;
+}
+
 void AnimationNodeAnimation::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_animation", "name"), &AnimationNodeAnimation::set_animation);
 	ClassDB::bind_method(D_METHOD("get_animation"), &AnimationNodeAnimation::get_animation);
 
+	ClassDB::bind_method(D_METHOD("set_play_mode", "mode"), &AnimationNodeAnimation::set_play_mode);
+	ClassDB::bind_method(D_METHOD("get_play_mode"), &AnimationNodeAnimation::get_play_mode);
+
 	ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "animation"), "set_animation", "get_animation");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "play_mode", PROPERTY_HINT_ENUM, "Forward,Backward"), "set_play_mode", "get_play_mode");
+
+	BIND_ENUM_CONSTANT(PLAY_MODE_FORWARD);
+	BIND_ENUM_CONSTANT(PLAY_MODE_BACKWARD);
 }
 
 AnimationNodeAnimation::AnimationNodeAnimation() {
@@ -533,7 +582,7 @@ AnimationNodeBlend3::AnimationNodeBlend3() {
 /////////////////////////////////
 
 void AnimationNodeTimeScale::get_parameter_list(List<PropertyInfo> *r_list) const {
-	r_list->push_back(PropertyInfo(Variant::FLOAT, scale, PROPERTY_HINT_RANGE, "0,32,0.01,or_greater"));
+	r_list->push_back(PropertyInfo(Variant::FLOAT, scale, PROPERTY_HINT_RANGE, "-32,32,0.01,or_lesser,or_greater"));
 }
 
 Variant AnimationNodeTimeScale::get_parameter_default_value(const StringName &p_parameter) const {

scene/animation/animation_player.h
@@ -215,7 +215,7 @@ private:
 
 	NodePath root;
 
-	void _animation_process_animation(AnimationData *p_anim, double p_time, double p_delta, float p_interp, bool p_is_current = true, bool p_seeked = false, bool p_started = false);
+	void _animation_process_animation(AnimationData *p_anim, double p_time, double p_delta, float p_interp, bool p_is_current = true, bool p_seeked = false, bool p_started = false, int p_pingponged = 0);
 
 	void _ensure_node_caches(AnimationData *p_anim, Node *p_root_override = nullptr);
 	void _animation_process_data(PlaybackData &cd, double p_delta, float p_blend, bool p_seeked, bool p_started);

scene/resources/animation.h
@@ -60,7 +60,12 @@ public:
 		UPDATE_DISCRETE,
 		UPDATE_TRIGGER,
 		UPDATE_CAPTURE,
+	};
 
+	enum LoopMode {
+		LOOP_NONE,
+		LOOP_LINEAR,
+		LOOP_PINGPONG,
 	};
 
 private:
@@ -184,7 +189,8 @@ private:
 	int _insert(double p_time, T &p_keys, const V &p_value);
 
 	template <class K>
-	inline int _find(const Vector<K> &p_keys, double p_time) const;
+
+	inline int _find(const Vector<K> &p_keys, double p_time, bool p_backward = false) const;
 
 	_FORCE_INLINE_ Animation::TransformKey _interpolate(const Animation::TransformKey &p_a, const Animation::TransformKey &p_b, real_t p_c) const;
 
@@ -200,7 +206,7 @@ private:
 	_FORCE_INLINE_ real_t _cubic_interpolate(const real_t &p_pre_a, const real_t &p_a, const real_t &p_b, const real_t &p_post_b, real_t p_c) const;
 
 	template <class T>
-	_FORCE_INLINE_ T _interpolate(const Vector<TKey<T>> &p_keys, double p_time, InterpolationType p_interp, bool p_loop_wrap, bool *p_ok) const;
+	_FORCE_INLINE_ T _interpolate(const Vector<TKey<T>> &p_keys, double p_time, InterpolationType p_interp, bool p_loop_wrap, bool *p_ok, bool p_backward = false) const;
 
 	template <class T>
 	_FORCE_INLINE_ void _track_get_key_indices_in_range(const Vector<T> &p_array, double from_time, double to_time, List<int> *p_indices) const;
@@ -210,15 +216,16 @@ private:
 
 	double length = 1.0;
 	real_t step = 0.1;
-	bool loop = false;
+	LoopMode loop_mode = LOOP_NONE;
+	int pingponged = 0;
 
 	// bind helpers
 private:
-	Array _transform_track_interpolate(int p_track, double p_time) const {
+	Array _transform_track_interpolate(int p_track, double p_time, bool p_backward = false) const {
 		Vector3 loc;
 		Quaternion rot;
 		Vector3 scale;
-		transform_track_interpolate(p_track, p_time, &loc, &rot, &scale);
+		transform_track_interpolate(p_track, p_time, &loc, &rot, &scale, p_backward);
 		Array ret;
 		ret.push_back(loc);
 		ret.push_back(rot);
@@ -324,26 +331,26 @@ public:
 	void track_set_interpolation_loop_wrap(int p_track, bool p_enable);
 	bool track_get_interpolation_loop_wrap(int p_track) const;
 
-	Error transform_track_interpolate(int p_track, double p_time, Vector3 *r_loc, Quaternion *r_rot, Vector3 *r_scale) const;
+	Error transform_track_interpolate(int p_track, double p_time, Vector3 *r_loc, Quaternion *r_rot, Vector3 *r_scale, bool p_backward = false) const;
 
 	Variant value_track_interpolate(int p_track, double p_time) const;
-	void value_track_get_key_indices(int p_track, double p_time, double p_delta, List<int> *p_indices) const;
+	void value_track_get_key_indices(int p_track, double p_time, double p_delta, List<int> *p_indices, int p_pingponged = 0) const;
 	void value_track_set_update_mode(int p_track, UpdateMode p_mode);
 	UpdateMode value_track_get_update_mode(int p_track) const;
 
-	void method_track_get_key_indices(int p_track, double p_time, double p_delta, List<int> *p_indices) const;
+	void method_track_get_key_indices(int p_track, double p_time, double p_delta, List<int> *p_indices, int p_pingponged = 0) const;
 	Vector<Variant> method_track_get_params(int p_track, int p_key_idx) const;
 	StringName method_track_get_name(int p_track, int p_key_idx) const;
 
 	void copy_track(int p_track, Ref<Animation> p_to_animation);
 
-	void track_get_key_indices_in_range(int p_track, double p_time, double p_delta, List<int> *p_indices) const;
+	void track_get_key_indices_in_range(int p_track, double p_time, double p_delta, List<int> *p_indices, int p_pingponged = 0) const;
 
 	void set_length(real_t p_length);
 	real_t get_length() const;
 
-	void set_loop(bool p_enabled);
-	bool has_loop() const;
+	void set_loop_mode(LoopMode p_loop_mode);
+	LoopMode get_loop_mode() const;
 
 	void set_step(real_t p_step);
 	real_t get_step() const;
@@ -359,5 +366,6 @@ public:
 VARIANT_ENUM_CAST(Animation::TrackType);
 VARIANT_ENUM_CAST(Animation::InterpolationType);
 VARIANT_ENUM_CAST(Animation::UpdateMode);
+VARIANT_ENUM_CAST(Animation::LoopMode);
 
 #endif