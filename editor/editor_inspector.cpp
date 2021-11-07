godotengine/godot

Unify determination of default property values

editor/editor_inspector.cpp
@@ -32,6 +32,7 @@
 
 #include "array_property_edit.h"
 #include "core/os/keyboard.h"
+#include "core/property_utils.h"
 #include "dictionary_property_edit.h"
 #include "editor/doc_tools.h"
 #include "editor_feature_profile.h"
@@ -396,177 +397,12 @@ bool EditorProperty::is_read_only() const {
 	return read_only;
 }
 
-bool EditorPropertyRevert::may_node_be_in_instance(Node *p_node) {
-	Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-
-	bool might_be = false;
-	Node *node = p_node;
-
-	while (node) {
-		if (node == edited_scene) {
-			if (node->get_scene_inherited_state().is_valid()) {
-				might_be = true;
-				break;
-			}
-			might_be = false;
-			break;
-		}
-		if (node->get_scene_instance_state().is_valid()) {
-			might_be = true;
-			break;
-		}
-		node = node->get_owner();
-	}
-
-	return might_be; // or might not be
-}
-
-bool EditorPropertyRevert::get_instantiated_node_original_property(Node *p_node, const StringName &p_prop, Variant &value, bool p_check_class_default) {
-	Node *node = p_node;
-	Node *orig = node;
-
-	Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-
-	bool found = false;
-
-	while (node) {
-		Ref<SceneState> ss;
-
-		if (node == edited_scene) {
-			ss = node->get_scene_inherited_state();
-
-		} else {
-			ss = node->get_scene_instance_state();
-		}
-
-		if (ss.is_valid()) {
-			NodePath np = node->get_path_to(orig);
-			int node_idx = ss->find_node_by_path(np);
-			if (node_idx >= 0) {
-				bool lfound = false;
-				Variant lvar;
-				lvar = ss->get_property_value(node_idx, p_prop, lfound);
-				if (lfound) {
-					found = true;
-					value = lvar;
-				}
-			}
-		}
-		if (node == edited_scene) {
-			//just in case
-			break;
-		}
-		node = node->get_owner();
-	}
-
-	if (p_check_class_default && !found && p_node) {
-		//if not found, try default class value
-		Variant attempt = ClassDB::class_get_default_property_value(p_node->get_class_name(), p_prop);
-		if (attempt.get_type() != Variant::NIL) {
-			found = true;
-			value = attempt;
-		}
-	}
-
-	return found;
-}
-
-bool EditorPropertyRevert::is_node_property_different(Node *p_node, const Variant &p_current, const Variant &p_orig) {
-	// this is a pretty difficult function, because a property may not be saved but may have
-	// the flag to not save if one or if zero
-
-	//make sure there is an actual state
-	{
-		Node *node = p_node;
-		if (!node) {
-			return false;
-		}
-
-		Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-		bool found_state = false;
-
-		while (node) {
-			Ref<SceneState> ss;
-
-			if (node == edited_scene) {
-				ss = node->get_scene_inherited_state();
-
-			} else {
-				ss = node->get_scene_instance_state();
-			}
-
-			if (ss.is_valid()) {
-				found_state = true;
-				break;
-			}
-			if (node == edited_scene) {
-				//just in case
-				break;
-			}
-			node = node->get_owner();
-		}
-
-		if (!found_state) {
-			return false; //pointless to check if we are not comparing against anything.
-		}
-	}
-
-	return is_property_value_different(p_current, p_orig);
-}
-
-bool EditorPropertyRevert::is_property_value_different(const Variant &p_a, const Variant &p_b) {
-	if (p_a.get_type() == Variant::FLOAT && p_b.get_type() == Variant::FLOAT) {
-		//this must be done because, as some scenes save as text, there might be a tiny difference in floats due to numerical error
-		return !Math::is_equal_approx((float)p_a, (float)p_b);
-	} else {
-		return p_a != p_b;
-	}
-}
-
 Variant EditorPropertyRevert::get_property_revert_value(Object *p_object, const StringName &p_property) {
-	// If the object implements property_can_revert, rely on that completely
-	// (i.e. don't then try to revert to default value - the property_get_revert implementation
-	// can do that if so desired)
 	if (p_object->has_method("property_can_revert") && p_object->call("property_can_revert", p_property)) {
 		return p_object->call("property_get_revert", p_property);
 	}
 
-	Ref<Script> scr = p_object->get_script();
-	Node *node = Object::cast_to<Node>(p_object);
-	if (node && EditorPropertyRevert::may_node_be_in_instance(node)) {
-		//if this node is an instance or inherits, but it has a script attached which is unrelated
-		//to the one set for the parent and also has a default value for the property, consider that
-		//has precedence over the value from the parent, because that is an explicit source of defaults
-		//closer in the tree to the current node
-		bool ignore_parent = false;
-		if (scr.is_valid()) {
-			Variant sorig;
-			if (EditorPropertyRevert::get_instantiated_node_original_property(node, "script", sorig) && !scr->inherits_script(sorig)) {
-				Variant dummy;
-				if (scr->get_property_default_value(p_property, dummy)) {
-					ignore_parent = true;
-				}
-			}
-		}
-
-		if (!ignore_parent) {
-			//check for difference including instantiation
-			Variant vorig;
-			if (EditorPropertyRevert::get_instantiated_node_original_property(node, p_property, vorig, false)) {
-				return vorig;
-			}
-		}
-	}
-
-	if (scr.is_valid()) {
-		Variant orig_value;
-		if (scr->get_property_default_value(p_property, orig_value)) {
-			return orig_value;
-		}
-	}
-
-	//report default class value instead
-	return ClassDB::class_get_default_property_value(p_object->get_class_name(), p_property);
+	return PropertyUtils::get_property_default_value(p_object, p_property);
 }
 
 bool EditorPropertyRevert::can_property_revert(Object *p_object, const StringName &p_property) {
@@ -575,7 +411,7 @@ bool EditorPropertyRevert::can_property_revert(Object *p_object, const StringNam
 		return false;
 	}
 	Variant current_value = p_object->get(p_property);
-	return EditorPropertyRevert::is_property_value_different(current_value, revert_value);
+	return PropertyUtils::is_property_value_different(current_value, revert_value);
 }
 
 void EditorProperty::update_reload_status() {
@@ -583,10 +419,10 @@ void EditorProperty::update_reload_status() {
 		return; //no property, so nothing to do
 	}
 
-	bool has_reload = EditorPropertyRevert::can_property_revert(object, property);
+	bool new_can_revert = EditorPropertyRevert::can_property_revert(object, property);
 
-	if (has_reload != can_revert) {
-		can_revert = has_reload;
+	if (new_can_revert != can_revert) {
+		can_revert = new_can_revert;
 		update();
 	}
 }

core/property_utils.cpp
@@ -1,0 +1,186 @@
+/*************************************************************************/
+/*  property_utils.cpp                                                   */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "property_utils.h"
+
+#include "core/config/engine.h"
+#include "core/templates/local_vector.h"
+#include "editor/editor_node.h"
+#include "scene/resources/packed_scene.h"
+
+bool PropertyUtils::is_property_value_different(const Variant &p_a, const Variant &p_b) {
+	if (p_a.get_type() == Variant::FLOAT && p_b.get_type() == Variant::FLOAT) {
+		//this must be done because, as some scenes save as text, there might be a tiny difference in floats due to numerical error
+		return !Math::is_equal_approx((float)p_a, (float)p_b);
+	} else {
+		return p_a != p_b;
+	}
+}
+
+Variant PropertyUtils::get_property_default_value(const Object *p_object, const StringName &p_property, const Node *p_owner, const Vector<SceneState::PackState> *p_states_stack_cache, bool *r_is_class_default) {
+	// This function obeys the way property values are set when an object is instantiated,
+	// which is the following (the latter wins):
+	// 1. Default value from builtin class
+	// 2. Default value from script exported variable (from the topmost script)
+	// 3. Value overrides from the instantiation/inheritance stack
+
+	if (r_is_class_default) {
+		*r_is_class_default = false;
+	}
+
+	Ref<Script> topmost_script;
+
+	if (const Node *node = Object::cast_to<Node>(p_object)) {
+		// Check inheritance/instantiation ancestors
+
+		const Node *owner = p_owner;
+#ifdef TOOLS_ENABLED
+		if (!p_owner && Engine::get_singleton()->is_editor_hint()) {
+			owner = EditorNode::get_singleton()->get_edited_scene();
+		}
+#endif
+		ERR_FAIL_COND_V(!owner, Variant());
+
+		const Vector<SceneState::PackState> &states_stack = p_states_stack_cache ? *p_states_stack_cache : PropertyUtils::get_node_states_stack(node, owner);
+		for (int i = 0; i < states_stack.size(); ++i) {
+			const SceneState::PackState &ia = states_stack[i];
+			bool found = false;
+			Variant value_in_ancestor = ia.state->get_property_value(ia.node, p_property, found);
+			if (found) {
+				return value_in_ancestor;
+			}
+			// Save script for later
+			bool has_script = false;
+			Variant script = ia.state->get_property_value(ia.node, "script", has_script);
+			if (has_script) {
+				Ref<Script> scr = script;
+				if (scr.is_valid()) {
+					topmost_script = scr;
+				}
+			}
+		}
+	}
+
+	// Let's see what default is set by the topmost script having a default, if any
+	if (topmost_script.is_null()) {
+		topmost_script = p_object->get_script();
+	}
+	if (topmost_script.is_valid()) {
+		Variant default_value;
+		if (topmost_script->get_property_default_value(p_property, default_value)) {
+			return default_value;
+		}
+	}
+
+	// Fall back to the default from the native class
+	if (r_is_class_default) {
+		*r_is_class_default = true;
+	}
+	return ClassDB::class_get_default_property_value(p_object->get_class_name(), p_property);
+}
+
+// Like SceneState::PackState, but using a raw pointer to avoid the cost of
+// updating the reference count during the internal work of the functions below
+namespace {
+struct _FastPackState {
+	SceneState *state = nullptr;
+	int node = -1;
+};
+} // namespace
+
+static bool _collect_inheritance_chain(const Ref<SceneState> &p_state, const NodePath &p_path, LocalVector<_FastPackState> &r_states_stack) {
+	bool found = false;
+
+	LocalVector<_FastPackState> inheritance_states;
+
+	Ref<SceneState> state = p_state;
+	while (state.is_valid()) {
+		int node = state->find_node_by_path(p_path);
+		if (node >= 0) {
+			// This one has state for this node
+			inheritance_states.push_back({ state.ptr(), node });
+			found = true;
+		}
+		state = state->get_base_scene_state();
+	}
+
+	for (int i = inheritance_states.size() - 1; i >= 0; --i) {
+		r_states_stack.push_back(inheritance_states[i]);
+	}
+
+	return found;
+}
+
+Vector<SceneState::PackState> PropertyUtils::get_node_states_stack(const Node *p_node, const Node *p_owner, bool *r_instantiated_by_owner) {
+	if (r_instantiated_by_owner) {
+		*r_instantiated_by_owner = true;
+	}
+
+	LocalVector<_FastPackState> states_stack;
+	{
+		const Node *owner = p_owner;
+#ifdef TOOLS_ENABLED
+		if (!p_owner && Engine::get_singleton()->is_editor_hint()) {
+			owner = EditorNode::get_singleton()->get_edited_scene();
+		}
+#endif
+
+		const Node *n = p_node;
+		while (n) {
+			if (n == owner) {
+				const Ref<SceneState> &state = n->get_scene_inherited_state();
+				if (_collect_inheritance_chain(state, n->get_path_to(p_node), states_stack)) {
+					if (r_instantiated_by_owner) {
+						*r_instantiated_by_owner = false;
+					}
+				}
+				break;
+			} else if (n->get_filename() != String()) {
+				const Ref<SceneState> &state = n->get_scene_instance_state();
+				_collect_inheritance_chain(state, n->get_path_to(p_node), states_stack);
+			}
+			n = n->get_owner();
+		}
+	}
+
+	// Convert to the proper type for returning, inverting the vector on the go
+	// (it was more convenient to fill the vector in reverse order)
+	Vector<SceneState::PackState> states_stack_ret;
+	{
+		states_stack_ret.resize(states_stack.size());
+		_FastPackState *ps = states_stack.ptr();
+		for (int i = states_stack.size() - 1; i >= 0; --i) {
+			states_stack_ret.write[i].state.reference_ptr(ps->state);
+			states_stack_ret.write[i].node = ps->node;
+			++ps;
+		}
+	}
+	return states_stack_ret;
+}

V-Sekai-fire/multiplayer-fabric-godot

editor/editor_inspector.cpp
@@ -39,6 +39,7 @@
 #include "editor_scale.h"
 #include "editor_settings.h"
 #include "multi_node_edit.h"
+#include "scene/property_utils.h"
 #include "scene/resources/packed_scene.h"
 
 Size2 EditorProperty::get_minimum_size() const {
@@ -398,177 +399,12 @@ bool EditorProperty::is_read_only() const {
 	return read_only;
 }
 
-bool EditorPropertyRevert::may_node_be_in_instance(Node *p_node) {
-	Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-
-	bool might_be = false;
-	Node *node = p_node;
-
-	while (node) {
-		if (node == edited_scene) {
-			if (node->get_scene_inherited_state().is_valid()) {
-				might_be = true;
-				break;
-			}
-			might_be = false;
-			break;
-		}
-		if (node->get_scene_instance_state().is_valid()) {
-			might_be = true;
-			break;
-		}
-		node = node->get_owner();
-	}
-
-	return might_be; // or might not be
-}
-
-bool EditorPropertyRevert::get_instantiated_node_original_property(Node *p_node, const StringName &p_prop, Variant &value, bool p_check_class_default) {
-	Node *node = p_node;
-	Node *orig = node;
-
-	Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-
-	bool found = false;
-
-	while (node) {
-		Ref<SceneState> ss;
-
-		if (node == edited_scene) {
-			ss = node->get_scene_inherited_state();
-
-		} else {
-			ss = node->get_scene_instance_state();
-		}
-
-		if (ss.is_valid()) {
-			NodePath np = node->get_path_to(orig);
-			int node_idx = ss->find_node_by_path(np);
-			if (node_idx >= 0) {
-				bool lfound = false;
-				Variant lvar;
-				lvar = ss->get_property_value(node_idx, p_prop, lfound);
-				if (lfound) {
-					found = true;
-					value = lvar;
-				}
-			}
-		}
-		if (node == edited_scene) {
-			//just in case
-			break;
-		}
-		node = node->get_owner();
-	}
-
-	if (p_check_class_default && !found && p_node) {
-		//if not found, try default class value
-		Variant attempt = ClassDB::class_get_default_property_value(p_node->get_class_name(), p_prop);
-		if (attempt.get_type() != Variant::NIL) {
-			found = true;
-			value = attempt;
-		}
-	}
-
-	return found;
-}
-
-bool EditorPropertyRevert::is_node_property_different(Node *p_node, const Variant &p_current, const Variant &p_orig) {
-	// this is a pretty difficult function, because a property may not be saved but may have
-	// the flag to not save if one or if zero
-
-	//make sure there is an actual state
-	{
-		Node *node = p_node;
-		if (!node) {
-			return false;
-		}
-
-		Node *edited_scene = EditorNode::get_singleton()->get_edited_scene();
-		bool found_state = false;
-
-		while (node) {
-			Ref<SceneState> ss;
-
-			if (node == edited_scene) {
-				ss = node->get_scene_inherited_state();
-
-			} else {
-				ss = node->get_scene_instance_state();
-			}
-
-			if (ss.is_valid()) {
-				found_state = true;
-				break;
-			}
-			if (node == edited_scene) {
-				//just in case
-				break;
-			}
-			node = node->get_owner();
-		}
-
-		if (!found_state) {
-			return false; //pointless to check if we are not comparing against anything.
-		}
-	}
-
-	return is_property_value_different(p_current, p_orig);
-}
-
-bool EditorPropertyRevert::is_property_value_different(const Variant &p_a, const Variant &p_b) {
-	if (p_a.get_type() == Variant::FLOAT && p_b.get_type() == Variant::FLOAT) {
-		//this must be done because, as some scenes save as text, there might be a tiny difference in floats due to numerical error
-		return !Math::is_equal_approx((float)p_a, (float)p_b);
-	} else {
-		return p_a != p_b;
-	}
-}
-
 Variant EditorPropertyRevert::get_property_revert_value(Object *p_object, const StringName &p_property) {
-	// If the object implements property_can_revert, rely on that completely
-	// (i.e. don't then try to revert to default value - the property_get_revert implementation
-	// can do that if so desired)
 	if (p_object->has_method("property_can_revert") && p_object->call("property_can_revert", p_property)) {
 		return p_object->call("property_get_revert", p_property);
 	}
 
-	Ref<Script> scr = p_object->get_script();
-	Node *node = Object::cast_to<Node>(p_object);
-	if (node && EditorPropertyRevert::may_node_be_in_instance(node)) {
-		//if this node is an instance or inherits, but it has a script attached which is unrelated
-		//to the one set for the parent and also has a default value for the property, consider that
-		//has precedence over the value from the parent, because that is an explicit source of defaults
-		//closer in the tree to the current node
-		bool ignore_parent = false;
-		if (scr.is_valid()) {
-			Variant sorig;
-			if (EditorPropertyRevert::get_instantiated_node_original_property(node, "script", sorig) && !scr->inherits_script(sorig)) {
-				Variant dummy;
-				if (scr->get_property_default_value(p_property, dummy)) {
-					ignore_parent = true;
-				}
-			}
-		}
-
-		if (!ignore_parent) {
-			//check for difference including instantiation
-			Variant vorig;
-			if (EditorPropertyRevert::get_instantiated_node_original_property(node, p_property, vorig, false)) {
-				return vorig;
-			}
-		}
-	}
-
-	if (scr.is_valid()) {
-		Variant orig_value;
-		if (scr->get_property_default_value(p_property, orig_value)) {
-			return orig_value;
-		}
-	}
-
-	//report default class value instead
-	return ClassDB::class_get_default_property_value(p_object->get_class_name(), p_property);
+	return PropertyUtils::get_property_default_value(p_object, p_property);
 }
 
 bool EditorPropertyRevert::can_property_revert(Object *p_object, const StringName &p_property) {
@@ -577,7 +413,7 @@ bool EditorPropertyRevert::can_property_revert(Object *p_object, const StringNam
 		return false;
 	}
 	Variant current_value = p_object->get(p_property);
-	return EditorPropertyRevert::is_property_value_different(current_value, revert_value);
+	return PropertyUtils::is_property_value_different(current_value, revert_value);
 }
 
 void EditorProperty::update_reload_status() {
@@ -585,10 +421,10 @@ void EditorProperty::update_reload_status() {
 		return; //no property, so nothing to do
 	}
 
-	bool has_reload = EditorPropertyRevert::can_property_revert(object, property);
+	bool new_can_revert = EditorPropertyRevert::can_property_revert(object, property);
 
-	if (has_reload != can_revert) {
-		can_revert = has_reload;
+	if (new_can_revert != can_revert) {
+		can_revert = new_can_revert;
 		update();
 	}
 }

scene/property_utils.cpp
@@ -1,0 +1,185 @@
+/*************************************************************************/
+/*  property_utils.cpp                                                   */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "property_utils.h"
+
+#include "core/config/engine.h"
+#include "core/templates/local_vector.h"
+#include "editor/editor_node.h"
+#include "scene/resources/packed_scene.h"
+
+bool PropertyUtils::is_property_value_different(const Variant &p_a, const Variant &p_b) {
+	if (p_a.get_type() == Variant::FLOAT && p_b.get_type() == Variant::FLOAT) {
+		//this must be done because, as some scenes save as text, there might be a tiny difference in floats due to numerical error
+		return !Math::is_equal_approx((float)p_a, (float)p_b);
+	} else {
+		// For our purposes, treating null object as NIL is the right thing to do
+		const Variant &a = p_a.get_type() == Variant::OBJECT && (Object *)p_a == nullptr ? Variant() : p_a;
+		const Variant &b = p_b.get_type() == Variant::OBJECT && (Object *)p_b == nullptr ? Variant() : p_b;
+		return a != b;
+	}
+}
+
+Variant PropertyUtils::get_property_default_value(const Object *p_object, const StringName &p_property, const Vector<SceneState::PackState> *p_states_stack_cache, bool p_update_exports, const Node *p_owner, bool *r_is_class_default) {
+	// This function obeys the way property values are set when an object is instantiated,
+	// which is the following (the latter wins):
+	// 1. Default value from builtin class
+	// 2. Default value from script exported variable (from the topmost script)
+	// 3. Value overrides from the instantiation/inheritance stack
+
+	if (r_is_class_default) {
+		*r_is_class_default = false;
+	}
+
+	Ref<Script> topmost_script;
+
+	if (const Node *node = Object::cast_to<Node>(p_object)) {
+		// Check inheritance/instantiation ancestors
+		const Vector<SceneState::PackState> &states_stack = p_states_stack_cache ? *p_states_stack_cache : PropertyUtils::get_node_states_stack(node, p_owner);
+		for (int i = 0; i < states_stack.size(); ++i) {
+			const SceneState::PackState &ia = states_stack[i];
+			bool found = false;
+			Variant value_in_ancestor = ia.state->get_property_value(ia.node, p_property, found);
+			if (found) {
+				return value_in_ancestor;
+			}
+			// Save script for later
+			bool has_script = false;
+			Variant script = ia.state->get_property_value(ia.node, SNAME("script"), has_script);
+			if (has_script) {
+				Ref<Script> scr = script;
+				if (scr.is_valid()) {
+					topmost_script = scr;
+				}
+			}
+		}
+	}
+
+	// Let's see what default is set by the topmost script having a default, if any
+	if (topmost_script.is_null()) {
+		topmost_script = p_object->get_script();
+	}
+	if (topmost_script.is_valid()) {
+		// Should be called in the editor only and not at runtime,
+		// otherwise it can cause problems because of missing instance state support
+		if (p_update_exports && Engine::get_singleton()->is_editor_hint()) {
+			topmost_script->update_exports();
+		}
+		Variant default_value;
+		if (topmost_script->get_property_default_value(p_property, default_value)) {
+			return default_value;
+		}
+	}
+
+	// Fall back to the default from the native class
+	if (r_is_class_default) {
+		*r_is_class_default = true;
+	}
+	return ClassDB::class_get_default_property_value(p_object->get_class_name(), p_property);
+}
+
+// Like SceneState::PackState, but using a raw pointer to avoid the cost of
+// updating the reference count during the internal work of the functions below
+namespace {
+struct _FastPackState {
+	SceneState *state = nullptr;
+	int node = -1;
+};
+} // namespace
+
+static bool _collect_inheritance_chain(const Ref<SceneState> &p_state, const NodePath &p_path, LocalVector<_FastPackState> &r_states_stack) {
+	bool found = false;
+
+	LocalVector<_FastPackState> inheritance_states;
+
+	Ref<SceneState> state = p_state;
+	while (state.is_valid()) {
+		int node = state->find_node_by_path(p_path);
+		if (node >= 0) {
+			// This one has state for this node
+			inheritance_states.push_back({ state.ptr(), node });
+			found = true;
+		}
+		state = state->get_base_scene_state();
+	}
+
+	for (int i = inheritance_states.size() - 1; i >= 0; --i) {
+		r_states_stack.push_back(inheritance_states[i]);
+	}
+
+	return found;
+}
+
+Vector<SceneState::PackState> PropertyUtils::get_node_states_stack(const Node *p_node, const Node *p_owner, bool *r_instantiated_by_owner) {
+	if (r_instantiated_by_owner) {
+		*r_instantiated_by_owner = true;
+	}
+
+	LocalVector<_FastPackState> states_stack;
+	{
+		const Node *owner = p_owner;
+#ifdef TOOLS_ENABLED
+		if (!p_owner && Engine::get_singleton()->is_editor_hint()) {
+			owner = EditorNode::get_singleton()->get_edited_scene();
+		}
+#endif
+
+		const Node *n = p_node;
+		while (n) {
+			if (n == owner) {
+				const Ref<SceneState> &state = n->get_scene_inherited_state();
+				if (_collect_inheritance_chain(state, n->get_path_to(p_node), states_stack)) {
+					if (r_instantiated_by_owner) {
+						*r_instantiated_by_owner = false;
+					}
+				}
+				break;
+			} else if (n->get_scene_file_path() != String()) {
+				const Ref<SceneState> &state = n->get_scene_instance_state();
+				_collect_inheritance_chain(state, n->get_path_to(p_node), states_stack);
+			}
+			n = n->get_owner();
+		}
+	}
+
+	// Convert to the proper type for returning, inverting the vector on the go
+	// (it was more convenient to fill the vector in reverse order)
+	Vector<SceneState::PackState> states_stack_ret;
+	{
+		states_stack_ret.resize(states_stack.size());
+		_FastPackState *ps = states_stack.ptr();
+		for (int i = states_stack.size() - 1; i >= 0; --i) {
+			states_stack_ret.write[i].state.reference_ptr(ps->state);
+			states_stack_ret.write[i].node = ps->node;
+			++ps;
+		}
+	}
+	return states_stack_ret;
+}