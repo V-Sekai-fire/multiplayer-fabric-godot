godotengine/godot

[Core] Allow locking/unlocking of `MutexLock`

core/os/mutex.h
@@ -74,11 +74,23 @@ template <typename MutexT>
 class MutexLock {
 	friend class ConditionVariable;
 
-	THREADING_NAMESPACE::unique_lock<typename MutexT::StdMutexType> lock;
+	mutable THREADING_NAMESPACE::unique_lock<typename MutexT::StdMutexType> _lock;
 
 public:
-	explicit MutexLock(const MutexT &p_mutex) :
-			lock(p_mutex.mutex) {}
+	_ALWAYS_INLINE_ explicit MutexLock(const MutexT &p_mutex) :
+			_lock(p_mutex.mutex) {}
+
+	_ALWAYS_INLINE_ void lock() const {
+		_lock.lock();
+	}
+
+	_ALWAYS_INLINE_ void unlock() const {
+		_lock.unlock();
+	}
+
+	_ALWAYS_INLINE_ bool try_lock() const {
+		return _lock.try_lock();
+	}
 };
 
 using Mutex = MutexImpl<THREADING_NAMESPACE::recursive_mutex>; // Recursive, for general use
@@ -104,6 +116,10 @@ template <typename MutexT>
 class MutexLock {
 public:
 	MutexLock(const MutexT &p_mutex) {}
+
+	void lock() const {}
+	void unlock() const {}
+	bool try_lock() const { return true; }
 };
 
 using Mutex = MutexImpl;

V-Sekai-fire/multiplayer-fabric-godot

core/os/mutex.h
@@ -72,7 +72,7 @@ public:
 
 template <typename MutexT>
 class MutexLock {
-	THREADING_NAMESPACE::unique_lock<typename MutexT::StdMutexType> lock;
+	mutable THREADING_NAMESPACE::unique_lock<typename MutexT::StdMutexType> lock;
 
 public:
 	explicit MutexLock(const MutexT &p_mutex) :
@@ -82,8 +82,18 @@ public:
 	template <typename T = MutexT>
 	_ALWAYS_INLINE_ THREADING_NAMESPACE::unique_lock<THREADING_NAMESPACE::mutex> &_get_lock(
 			typename std::enable_if<std::is_same<T, THREADING_NAMESPACE::mutex>::value> * = nullptr) const {
-		return const_cast<THREADING_NAMESPACE::unique_lock<THREADING_NAMESPACE::mutex> &>(lock);
+		return lock;
 	}
+
+	_ALWAYS_INLINE_ void temp_relock() const {
+		lock.lock();
+	}
+
+	_ALWAYS_INLINE_ void temp_unlock() const {
+		lock.unlock();
+	}
+
+	// TODO: Implement a `try_temp_relock` if needed (will also need a dummy method below).
 };
 
 using Mutex = MutexImpl<THREADING_NAMESPACE::recursive_mutex>; // Recursive, for general use
@@ -109,6 +119,9 @@ template <typename MutexT>
 class MutexLock {
 public:
 	MutexLock(const MutexT &p_mutex) {}
+
+	void temp_relock() const {}
+	void temp_unlock() const {}
 };
 
 using Mutex = MutexImpl;