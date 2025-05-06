okamstudio/godot

Inline `String::utf8` and `String::utf16` for their simplicity.

core/string/ustring.h
@@ -523,11 +523,19 @@ public:
 	CharString ascii(bool p_allow_extended = false) const;
 	CharString utf8() const;
 	Error parse_utf8(const char *p_utf8, int p_len = -1, bool p_skip_cr = false);
-	static String utf8(const char *p_utf8, int p_len = -1);
+	static String utf8(const char *p_utf8, int p_len = -1) {
+		String ret;
+		ret.parse_utf8(p_utf8, p_len);
+		return ret;
+	}
 
 	Char16String utf16() const;
 	Error parse_utf16(const char16_t *p_utf16, int p_len = -1, bool p_default_little_endian = true);
-	static String utf16(const char16_t *p_utf16, int p_len = -1);
+	static String utf16(const char16_t *p_utf16, int p_len = -1) {
+		String ret;
+		ret.parse_utf16(p_utf16, p_len, true);
+		return ret;
+	}
 
 	static uint32_t hash(const char32_t *p_cstr, int p_len); /* hash the string */
 	static uint32_t hash(const char32_t *p_cstr); /* hash the string */

V-Sekai-fire/multiplayer-fabric-godot

core/string/ustring.h
@@ -545,7 +545,11 @@ public:
 	Error append_utf8(const Span<char> &p_range, bool p_skip_cr = false) {
 		return append_utf8(p_range.ptr(), p_range.size(), p_skip_cr);
 	}
-	static String utf8(const char *p_utf8, int p_len = -1);
+	static String utf8(const char *p_utf8, int p_len = -1) {
+		String ret;
+		ret.append_utf8(p_utf8, p_len);
+		return ret;
+	}
 	static String utf8(const Span<char> &p_range) { return utf8(p_range.ptr(), p_range.size()); }
 
 	Char16String utf16() const;
@@ -553,7 +557,11 @@ public:
 	Error append_utf16(const Span<char16_t> p_range, bool p_skip_cr = false) {
 		return append_utf16(p_range.ptr(), p_range.size(), p_skip_cr);
 	}
-	static String utf16(const char16_t *p_utf16, int p_len = -1);
+	static String utf16(const char16_t *p_utf16, int p_len = -1) {
+		String ret;
+		ret.append_utf16(p_utf16, p_len);
+		return ret;
+	}
 	static String utf16(const Span<char16_t> &p_range) { return utf16(p_range.ptr(), p_range.size()); }
 
 	void append_utf32(const Span<char32_t> &p_cstr);