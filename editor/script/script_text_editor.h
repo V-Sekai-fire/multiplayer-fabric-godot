godotengine/godot

Use only one edit menu for TextEditors

editor/script/script_text_editor.h
@@ -57,6 +57,28 @@ public:
 class ScriptTextEditor : public ScriptEditorBase {
 	GDCLASS(ScriptTextEditor, ScriptEditorBase);
 
+	class EditMenus : public HBoxContainer {
+		MenuButton *edit_menu = nullptr;
+		MenuButton *search_menu = nullptr;
+		MenuButton *goto_menu = nullptr;
+		PopupMenu *bookmarks_menu = nullptr;
+		PopupMenu *breakpoints_menu = nullptr;
+		PopupMenu *highlighter_menu = nullptr;
+
+		ScriptTextEditor *_get_active_editor();
+		void _edit_option(int p_op);
+		void _prepare_edit_menu();
+		void _update_highlighter_menu();
+		void _change_syntax_highlighter(int p_idx);
+		void _update_bookmark_list();
+		void _bookmark_item_pressed(int p_idx);
+		void _update_breakpoint_list();
+		void _breakpoint_item_pressed(int p_idx);
+
+	public:
+		EditMenus();
+	};
+
 	CodeTextEditor *code_editor = nullptr;
 	RichTextLabel *warnings_panel = nullptr;
 	RichTextLabel *errors_panel = nullptr;
@@ -76,14 +98,7 @@ class ScriptTextEditor : public ScriptEditorBase {
 
 	Vector<String> member_keywords;
 
-	HBoxContainer *edit_hb = nullptr;
-
-	MenuButton *edit_menu = nullptr;
-	MenuButton *search_menu = nullptr;
-	MenuButton *goto_menu = nullptr;
-	PopupMenu *bookmarks_menu = nullptr;
-	PopupMenu *breakpoints_menu = nullptr;
-	PopupMenu *highlighter_menu = nullptr;
+	static inline EditMenus *edit_menus = nullptr;
 	PopupMenu *context_menu = nullptr;
 
 	int inline_color_line = -1;
@@ -117,6 +132,8 @@ class ScriptTextEditor : public ScriptEditorBase {
 	String color_args;
 
 	bool theme_loaded = false;
+
+	LocalVector<Ref<EditorSyntaxHighlighter>> highlighters;
 
 	enum {
 		EDIT_UNDO,
@@ -194,8 +211,6 @@ class ScriptTextEditor : public ScriptEditorBase {
 	void _assign_dragged_export_variables();
 
 protected:
-	void _update_breakpoint_list();
-	void _breakpoint_item_pressed(int p_idx);
 	void _breakpoint_toggled(int p_row);
 
 	void _on_caret_moved();
@@ -203,8 +218,6 @@ protected:
 	void _validate_script(); // No longer virtual.
 	void _update_warnings();
 	void _update_errors();
-	void _update_bookmark_list();
-	void _bookmark_item_pressed(int p_idx);
 
 	static void _code_complete_scripts(void *p_ud, const String &p_code, List<ScriptLanguage::CodeCompletionOption> *r_options, bool &r_force);
 	void _code_complete_script(const String &p_code, List<ScriptLanguage::CodeCompletionOption> *r_options, bool &r_force);
@@ -228,15 +241,11 @@ protected:
 
 	void _notification(int p_what);
 
-	HashMap<String, Ref<EditorSyntaxHighlighter>> highlighters;
-	void _change_syntax_highlighter(int p_idx);
-
 	void _edit_option(int p_op);
 	void _edit_option_toggle_inline_comment();
 	void _make_context_menu(bool p_selection, bool p_color, bool p_foldable, bool p_open_docs, bool p_goto_definition, Vector2 p_pos);
 	void _text_edit_gui_input(const Ref<InputEvent> &ev);
 	void _color_changed(const Color &p_color);
-	void _prepare_edit_menu();
 
 	void _goto_line(int p_line) { goto_line(p_line); }
 	void _lookup_symbol(const String &p_symbol, int p_row, int p_column);
@@ -262,7 +271,7 @@ public:
 	virtual void apply_code() override;
 	virtual Ref<Resource> get_edited_resource() const override;
 	virtual void set_edited_resource(const Ref<Resource> &p_res) override;
-	virtual void enable_editor(Control *p_shortcut_context = nullptr) override;
+	virtual void enable_editor() override;
 	virtual Vector<String> get_functions() override;
 	virtual void reload_text() override;
 	virtual String get_name() override;
@@ -298,8 +307,7 @@ public:
 
 	virtual void set_debugger_active(bool p_active) override;
 
-	Control *get_edit_menu() override;
-	virtual void clear_edit_menu() override;
+	virtual Control *get_edit_menu() override;
 	virtual void set_find_replace_bar(FindReplaceBar *p_bar) override;
 
 	static void register_editor();

V-Sekai-fire/multiplayer-fabric-godot

editor/script/script_text_editor.h
@@ -57,6 +57,29 @@ public:
 class ScriptTextEditor : public ScriptEditorBase {
 	GDCLASS(ScriptTextEditor, ScriptEditorBase);
 
+	class EditMenusSTE : public HBoxContainer {
+		GDCLASS(EditMenusSTE, HBoxContainer);
+		MenuButton *edit_menu = nullptr;
+		MenuButton *search_menu = nullptr;
+		MenuButton *goto_menu = nullptr;
+		PopupMenu *bookmarks_menu = nullptr;
+		PopupMenu *breakpoints_menu = nullptr;
+		PopupMenu *highlighter_menu = nullptr;
+
+		ScriptTextEditor *_get_active_editor();
+		void _edit_option(int p_op);
+		void _prepare_edit_menu();
+		void _update_highlighter_menu();
+		void _change_syntax_highlighter(int p_idx);
+		void _update_bookmark_list();
+		void _bookmark_item_pressed(int p_idx);
+		void _update_breakpoint_list();
+		void _breakpoint_item_pressed(int p_idx);
+
+	public:
+		EditMenusSTE();
+	};
+
 	CodeTextEditor *code_editor = nullptr;
 	RichTextLabel *warnings_panel = nullptr;
 	RichTextLabel *errors_panel = nullptr;
@@ -74,14 +97,7 @@ class ScriptTextEditor : public ScriptEditorBase {
 
 	List<Connection> missing_connections;
 
-	HBoxContainer *edit_hb = nullptr;
-
-	MenuButton *edit_menu = nullptr;
-	MenuButton *search_menu = nullptr;
-	MenuButton *goto_menu = nullptr;
-	PopupMenu *bookmarks_menu = nullptr;
-	PopupMenu *breakpoints_menu = nullptr;
-	PopupMenu *highlighter_menu = nullptr;
+	static inline EditMenusSTE *edit_menus = nullptr;
 	PopupMenu *context_menu = nullptr;
 
 	int inline_color_line = -1;
@@ -115,6 +131,8 @@ class ScriptTextEditor : public ScriptEditorBase {
 	String color_args;
 
 	bool theme_loaded = false;
+
+	LocalVector<Ref<EditorSyntaxHighlighter>> highlighters;
 
 	enum {
 		EDIT_UNDO,
@@ -193,8 +211,6 @@ class ScriptTextEditor : public ScriptEditorBase {
 	void _assign_dragged_export_variables();
 
 protected:
-	void _update_breakpoint_list();
-	void _breakpoint_item_pressed(int p_idx);
 	void _breakpoint_toggled(int p_row);
 
 	void _on_caret_moved();
@@ -202,8 +218,6 @@ protected:
 	void _validate_script(); // No longer virtual.
 	void _update_warnings();
 	void _update_errors();
-	void _update_bookmark_list();
-	void _bookmark_item_pressed(int p_idx);
 
 	static void _code_complete_scripts(void *p_ud, const String &p_code, List<ScriptLanguage::CodeCompletionOption> *r_options, bool &r_force);
 	void _code_complete_script(const String &p_code, List<ScriptLanguage::CodeCompletionOption> *r_options, bool &r_force);
@@ -227,15 +241,11 @@ protected:
 
 	void _notification(int p_what);
 
-	HashMap<String, Ref<EditorSyntaxHighlighter>> highlighters;
-	void _change_syntax_highlighter(int p_idx);
-
 	void _edit_option(int p_op);
 	void _edit_option_toggle_inline_comment();
 	void _make_context_menu(bool p_selection, bool p_color, bool p_foldable, bool p_open_docs, bool p_goto_definition, Vector2 p_pos);
 	void _text_edit_gui_input(const Ref<InputEvent> &ev);
 	void _color_changed(const Color &p_color);
-	void _prepare_edit_menu();
 
 	void _goto_line(int p_line) { goto_line(p_line); }
 	void _lookup_symbol(const String &p_symbol, int p_row, int p_column);
@@ -261,7 +271,7 @@ public:
 	virtual void apply_code() override;
 	virtual Ref<Resource> get_edited_resource() const override;
 	virtual void set_edited_resource(const Ref<Resource> &p_res) override;
-	virtual void enable_editor(Control *p_shortcut_context = nullptr) override;
+	virtual void enable_editor() override;
 	virtual Vector<String> get_functions() override;
 	virtual void reload_text() override;
 	virtual String get_name() override;
@@ -297,8 +307,7 @@ public:
 
 	virtual void set_debugger_active(bool p_active) override;
 
-	Control *get_edit_menu() override;
-	virtual void clear_edit_menu() override;
+	virtual Control *get_edit_menu() override;
 	virtual void set_find_replace_bar(FindReplaceBar *p_bar) override;
 
 	static void register_editor();