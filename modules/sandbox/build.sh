46deb71a1e9187500974d28a0c0ff4d60ff40b61
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-18 22:48:55 -0700

    Add feat/module-sandbox

---
+++ modules/sandbox/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+mkdir -p .build
+pushd .build
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
+ninja
+popd
---
+++ modules/sandbox/program/rust/docker/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PROJECT_PATH=${1:-.}
+pushd $PROJECT_PATH
+docker run --name godot-rust-compiler -dv .:/usr/src riscv64-rust
+popd
