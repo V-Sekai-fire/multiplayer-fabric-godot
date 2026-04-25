153f075f784b6a7504bb189a00e05057edb05b9c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-24 17:06:05 -0700

    fix: remove duplicate return in can_build

--- modules/multiplayer_fabric/config.py
+++ modules/multiplayer_fabric/config.py
@@ -6,9 +6,6 @@ def can_build(env, platform):
         and env.get("module_http3_enabled", False)
         and os.path.isfile("scene/main/fabric_zone_peer_callbacks.h")
     )
-
-    return (
-        env.get("module_sqlite_enabled", False)
         and env.get("module_http3_enabled", False)
         and os.path.isfile("scene/main/fabric_zone_peer_callbacks.h")
     )
